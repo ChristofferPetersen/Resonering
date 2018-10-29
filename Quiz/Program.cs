0644864ba21119c37aba536facea41fe7049c4f2
malthe <malthe.dk@hotmail.com>
2018-10-29 11:23:54 +0100

    sup

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +11,7 @@ namespace Quiz
         static void Main(string[] args)
         {
             Console.WriteLine("Dette er FUCKED");
+             Console.WriteLine("Nej det er ej");
         }
     }
 }
