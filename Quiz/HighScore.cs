349a4d5293d7f2673a43bff4302cae53eb3ed79e
Ian <ikfa.skp@edu.mercantec.dk>
2018-10-30 12:19:32 +0100

    et eller andet

--- Quiz/HighScore.cs
+++ Quiz/HighScore.cs
@@ -16,31 +16,51 @@ namespace Quiz
         public DataGroup HighScoreModule(DataGroup datagroup)
         {
             //Console.WriteLine(datagroup.newScore);
-            readFile(datagroup);
+            List<string> highscorelist = new List<string>();
+            highscorelist=readFile(datagroup);
+
+            datagroup.newScore = 5;
+
+            highscorelist =checkScore(highscorelist, datagroup.newScore);
             return datagroup;
         }
 
-        public string readFile(DataGroup dataGroup)
+        public List<string> readFile(DataGroup dataGroup)
         {
+            List<string> highscorelist = new List<string>();
             string file = "../../highscore.txt";
             if (File.Exists(file))
             {
                 string fileContent = File.ReadAllText(file).Replace("\r", "");
-                //string[] lines = fileContent.Split("\n");
-                //foreach (string value in lines) Console.WriteLine(value+"\n");
+                string[] lines = fileContent.Split('\n');
+                foreach (string value in lines)
+                {
+                    Console.WriteLine(value.Split(',')[0] + " - " + value.Split(',')[1]);
+                    highscorelist.Add(value);
+                }
             }
             else
                 File.OpenWrite(file);
 
-            return "";
+            return highscorelist;
         }
-        /*
-        public void writeFile(DataGroup dataGroup)
+
+        public List<string> checkScore(List<string> highscorelist,int score)
         {
-            string[] listeArr = dataGroup.list.Split(",");
-            //foreach (string value in listeArr) File.AppendAllText(dataGroup.path+dataGroup.filename,value+"\n");
-            File.WriteAllLines(dataGroup.path + dataGroup.filename, listeArr);
+            List<string> NyHighScoreList = new List<string>();
+            foreach (string linje in highscorelist)
+            {
+                //if(linje.Split[1]<score)
+            }
+            return highscorelist;
         }
-        */
-    }
+            /*
+            public void writeFile(DataGroup dataGroup)
+            {
+                string[] listeArr = dataGroup.list.Split(",");
+                //foreach (string value in listeArr) File.AppendAllText(dataGroup.path+dataGroup.filename,value+"\n");
+                File.WriteAllLines(dataGroup.path + dataGroup.filename, listeArr);
+            }
+            */
+        }
 }
